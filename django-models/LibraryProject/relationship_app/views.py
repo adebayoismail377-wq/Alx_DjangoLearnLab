1f4fbc4c6c01bf177cfbcbb9c5d8dcc4af5debfe
Adebayo Ismail <adebayoismail377@gmail.com>
2026-01-20 18:37:36 +0100

    adjusted the list_books.html

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from .models import Book
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 # Create your views here.
 
 from django.views.generic import DetailView
@@ -13,7 +13,7 @@ from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'  # This makes {{ library }} available in the template
 
     # Note: In the template, you can access all books in the library with library.books.al
