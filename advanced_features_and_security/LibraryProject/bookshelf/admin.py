5995c69171d2a51598df28fd2125f264802b5b3f
Adebayo Ismail <adebayoismail377@gmail.com>
2026-01-22 14:49:36 +0100

    user

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Book
+from .models import CustomUser
 
+admin.site.register(CustomUser)
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
