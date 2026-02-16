1f9e6cd9b64861842cd807e4ce456e641eba7f06
Adebayo Ismail <adebayoismail377@gmail.com>
2026-02-17 00:42:28 +0100

    added database

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog',
+        'USER': 'root',              # or your MySQL username
+        'PASSWORD': 'Luminousphotography;1', # your MySQL password
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
