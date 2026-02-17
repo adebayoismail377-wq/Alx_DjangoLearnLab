230092dc9bf128e3fbbf9a5e6e6f31ef345882e3
Adebayo Ismail <adebayoismail377@gmail.com>
2026-02-17 17:48:41 +0100

    views and forms created on blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm, UpdateUserForm
 
 def home(request):
     return render(request, 'blog/home.html')
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'blog/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UpdateUserForm(request.POST, instance=request.user)
+
+        if u_form.is_valid():
+            u_form.save()
+            messages.success(request, 'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UpdateUserForm(instance=request.user)
+
+    context = {
+        'u_form': u_form
+    }
+
+    return render(request, 'blog/profile.html', context)
+
+
